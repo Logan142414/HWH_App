fbc087d671ca730a8a7b18a063d03e96f637f2dc
Logan Laszewski <logan.laszewski@comcast.net>
2025-04-21 16:03:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,8 +88,6 @@ if st.button("Predict Probabilities"):
 
     # --- Preview Period Prediction ---
     prob_preview = model.predict_proba(input_df)[0][1]
-    
-with st.container():
     st.markdown("### 📊 Preview Period Prediction")
     if prob_preview > 0.8:
         st.success(f"High chance of passing preview: {prob_preview:.2%}")
