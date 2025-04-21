645e4b99208a8886e1ee48e4d0b615693d06672a
Logan Laszewski <logan.laszewski@comcast.net>
2025-04-21 14:09:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ featureSSF = ['ssf_initial:adult_education', 'ssf_initial:child_care',
        'ssf_initial:social', 'ssf_initial:substance_abuse']
 
 # --- Predict ---
-if st.button("Predict Probability"):
+if st.button("Predict Probability for Passing Preview Period (>12)"):
     input_df = pd.DataFrame([features], columns=featureSSF)
     prob = model.predict_proba(input_df)[0][1]
     st.success(f"Predicted Probability: {prob:.2%}")
